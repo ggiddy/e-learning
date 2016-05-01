64098c5e03d6f40f9c4703351264df29cabdcc5e
ggiddy <nggitau@gmail.com>
2016-05-01 15:34:02 +0300

    No commit message

--- models/class.js
+++ models/class.js
@@ -3,8 +3,6 @@
 var mongoose = require('mongoose');
 
 var classSchema = new mongoose.Schema({
-    
-    //_id will be used by socket.io to join students of the same class together
     class_name: {type: String, required: true}, 
     class_code: {type: String, required: true, index: {unique: true}},
     instructor: {type: mongoose.Schema.Types.ObjectId, ref: 'Instructor'},
